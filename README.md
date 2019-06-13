075178197feab337b20f3b371ca815a97385a60d
Mike Rossetti <s2t2@users.noreply.github.com>
2019-06-13 00:19:23 -0400

    Send email with static data

--- README.md
+++ README.md
@@ -4,10 +4,38 @@
 
 ## Installation
 
-TODO
+Fork [this repo](https://github.com/s2t2/shopping-cart-with-email-receipts), then clone your fork to download it locally onto your computer. Then navigate there from the command-line:
+
+```sh
+cd shopping-cart-with-email-receipts
+```
+
+## Setup
+
+### Credentials Setup
+
+Follow [this tutorial](https://github.com/prof-rossetti/nyu-info-2335-201905/blob/master/notes/python/packages/sendgrid.md) to sign up for a Sendgrid API Key and configure your own Sendgrid Email Template, then locate the template's unique identifier.
+
+Create a new file called ".env" in the root directory of this repo, then copy the contents of the "env.example" file into it. Then adapt the values in the ".env" file to specify your own `SENDGRID_API_KEY`,
+`SENDGRID_TEMPLATE_ID`, and `EMAIL_ADDRESS` values, respectively.
+
+### Environment Setup
+
+Setup a virtual environment called something like "emails-env" and from within it install a specific version of the `sendgrid` package, which we'll use to send the emails:
+
+```sh
+conda create emails-env python=3.7 # (first time only)
+conda activate emails-env
+
+pip install sendgrid==6.0.5
+```
 
 ## Usage
+
+Run the checkout process:
 
 ```sh
 python checkout.py
 ```
+
+## [License](/LICENSE.md)
--- checkout.py
+++ checkout.py
@@ -1,5 +1,16 @@
 
 
+import os
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+load_dotenv() # loads environment variables from the ".env" file
+
+SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY", "OOPS, please set env var called 'SENDGRID_API_KEY'")
+SENDGRID_TEMPLATE_ID = os.environ.get("SENDGRID_TEMPLATE_ID", "OOPS, please set env var called 'SENDGRID_TEMPLATE_ID'")
+EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS", "OOPS, please set env var called 'EMAIL_ADDRESS'")
+
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
     {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
@@ -23,10 +34,15 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
+#
+# CAPTURE AND VALIDATE USER SELECTIONS
+#
+
 valid_ids = [str(p["id"]) for p in products] # doing comparisons with string versions of these ids
 print("VALID IDS:", valid_ids)
 
-selected_ids = []
+#selected_ids = []
+selected_products = [] # capturing products instead of ids so I can pass them to the email template
 
 while True:
     selected_id = input("Please input a product identifier, or 'DONE': " ) # the data input will always be a str
@@ -34,9 +50,48 @@ while True:
     if selected_id.upper() == "DONE":
         break # stops the loop
     elif selected_id in valid_ids:
-        selected_ids.append(selected_id)
+        #selected_ids.append(selected_id)
+        matching_product = [p for p in products if str(p["id"] == str(selected_id))][0] # can use [0] because we already validated the id, and we can expect there not to be a key error, otherwise we'd have to use a try... except block to handle the KeyError
+        selected_products.append(matching_product)
     else:
         print("OH, detected invalid input! Please try again...")
         next # proceeds into the next iteration of the loop (OK to omit in this basic example because there is no more code following it inside the loop before the loop repeats)
 
-print("SELECTED IDS:", selected_ids)
+#print("SELECTED IDS:", selected_ids)
+
+#
+# SEND RECEIPT VIA SENDGRID "TEMPLATE EMAIL"
+# see: https://github.com/prof-rossetti/nyu-info-2335-201905/blob/master/notes/python/packages/sendgrid.md
+#
+
+template_data = {
+    "total_price_usd": "$14.95",
+    "human_friendly_timestamp": "June 1st, 2019 10:00 AM",
+    "products":[
+        {"id":1, "name": "Product 1"},
+        {"id":2, "name": "Product 2"},
+        {"id":3, "name": "Product 3"},
+        {"id":2, "name": "Product 2"},
+        {"id":1, "name": "Product 1"}
+    ]
+}
+
+client = SendGridAPIClient(SENDGRID_API_KEY)
+print("CLIENT:", type(client))
+
+message = Mail(from_email=EMAIL_ADDRESS, to_emails=EMAIL_ADDRESS)
+print("MESSAGE:", type(message))
+
+message.template_id = SENDGRID_TEMPLATE_ID
+
+message.dynamic_template_data = template_data
+
+try:
+    response = client.send(message)
+    print("RESPONSE:", type(response))
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+
+except Exception as e:
+    print("OOPS", e)
